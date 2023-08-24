9918fd1bfc6f9529ce6d66d1377aef8494927556
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-24 22:53:12 +0530

    Add: Prompt Card list

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,106 @@
-import React from 'react'
+"use client"
+import { useState, useEffect } from "react"
+
+import PromptCard from "./PromptCard"
+import { fetchData } from "next-auth/client/_utils"
+import { set } from "mongoose"
+
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className="mt-16 prompt_layout">
+
+            {
+                data.map((post) => (
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleTagClick={handleTagClick}
+
+                    />
+                ))
+            }
+
+        </div>
+    )
+}
+
+
+
 
 const Feed = () => {
+
+    const [searchText, setSearchText] = useState("")
+    const [posts, setPosts] = useState([])
+
+
+    const handleTagClick = (tag) => {
+        setSearchText(tag)
+    }
+
+
+    const handleSearchChange = async (e) => {
+        e.preventDefault();
+
+        if (searchText === "") {
+            fetchPosts()
+        }
+
+        const response = await fetch('/api/prompt/search', {
+            method: 'POST',
+            body: JSON.stringify({
+                search: searchText
+            }),
+        })
+        const data = await response.json()
+
+
+        setPosts(data)
+
+        setSearchText("")
+
+
+    }
+
+    const fetchPosts = async () => {
+        const response = await fetch('/api/prompt');
+        const data = await response.json()
+        setPosts(data)
+
+    }
+
+    useEffect(() => {
+
+        fetchPosts()
+    }, [])
+
+
     return (
-        <div>Feed</div>
+        <section className="feed">
+            <form action="" className="relative w-full flex-center" onSubmit={handleSearchChange}>
+
+                <input
+                    type="text"
+                    placeholder="Search for a tag or a username"
+                    name="search"
+                    className="search_input peer"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+
+                />
+                <button type="submit" className="px-4 mx-2 py-2 bg-orange-500 rounded-lg">
+                    Search
+                </button>
+
+            </form>
+
+
+            <PromptCardList data={posts} handleTagClick={handleTagClick}
+            />
+
+
+
+        </section>
     )
 }
 
